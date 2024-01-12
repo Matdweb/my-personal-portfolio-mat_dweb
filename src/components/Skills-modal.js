d4dd83feec4b6e7247b52f0f9b0d2475380659c8
Matias Vargas <mat.dweb28[1>
2024-01-11 18:37:03 -0600

    feat: add new skills to SkillsModal

--- src/components/Skills-modal.js
+++ src/components/Skills-modal.js
@@ -3,18 +3,22 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { useContext } from 'react';
-import {SkillsContext} from "../context/SkillsContext"
+import { SkillsContext } from "../context/SkillsContext"
 //Images importation
 import HTML from "../assets/img/html-5.png";
-import WebD from "../assets/img/Web Developer_Monochromatic.svg";
 import CSS from "../assets/img/css-3.png";
 import JS from "../assets/img/js.png";
-import react from "../assets/img/react-logo.svg";
-import Design from "../assets/img/layers.png";
-import MUI from "../assets/img/material-ui-1.svg";
+import REACT from "../assets/img/react-logo.svg";
+import DESING_UI from "../assets/img/layers.png";
 import VITEJS from "../assets/img/vitejs.svg";
-import FIGMA from '../assets/img/figma-logo.svg'
-import CHAKRA from '../assets/img/icons8-chakra-ui.svg'
+import FIGMA from '../assets/img/figma-logo.svg';
+import TYPESCRIPT from '../assets/img/icons8-typescript-96.png'
+import EXPRESS from '../assets/img/icons8-express-js-100.png'
+import JAVA from '../assets/img/icons8-java-94.png'
+import MONGODB from '../assets/img/icons8-mongodb-a-cross-platform-document-oriented-database-program-96.png'
+import NEXTJS from '../assets/img/icons8-nextjs-96.png'
+import PHYTON from '../assets/img/icons8-python.svg'
+import REDUX from '../assets/img/icons8-redux-an-open-source-javascript-library-for-managing-application-state-96.png'
 
 const style = {
   position: 'absolute',
@@ -32,7 +36,7 @@ const style = {
 export function BasicModal() {
 
   const context = useContext(SkillsContext)
-  const skills = [HTML,WebD,CSS,JS,react,Design,MUI,VITEJS,FIGMA,CHAKRA]
+  const skills = [HTML, CSS, JS, TYPESCRIPT, REACT, VITEJS, NEXTJS, REDUX, EXPRESS, MONGODB, FIGMA, DESING_UI, JAVA, PHYTON]
   return (
     <div>
       <Modal
@@ -41,18 +45,18 @@ export function BasicModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} style={window.screen.width < 510 ? {width: "320px"} : {width: "500px"}}>
+        <Box sx={style} style={window.screen.width < 510 ? { width: "320px" } : { width: "500px" }}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Skills Percentaje
+            Skillset
           </Typography>
           <div className='modal-skills'>
-              {skills.map((skill,index)=>{
-                return (
-                  <div key={index}>
-                    <img src={skill}/>
-                  </div>
-                )
-              })}
+            {skills.map((skill, index) => {
+              return (
+                <div key={index}>
+                  <img src={skill} />
+                </div>
+              )
+            })}
           </div>
         </Box>
       </Modal>
