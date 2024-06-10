ca2bebebd4aabad0ac203fcba1ad7c8df15cbde9
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-06-09 20:30:01 -0600

    feat: add tech stack to each project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,19 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, URL }) => {
+export const ProjectCard = ({ title, description, imgUrl, techStack = [], URL }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
-        <a href={URL} style={{textDecoration: "none", color: "#fff", cursor: "pointer"}} className="proj-txtx">
+        <a href={URL} style={{ textDecoration: "none", color: "#fff", cursor: "pointer" }} className="proj-txtx">
           <h4>{title}</h4>
+          <div className="proj-tech-stack">
+            {techStack?.map((tech, i) => {
+              return (
+                <span key={i} className="proj-tech">{tech}</span>
+              )
+            })}
+          </div>
           <span>{description}</span>
         </a>
       </div>
